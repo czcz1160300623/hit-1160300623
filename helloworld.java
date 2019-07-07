7bdbc02ba813cda6c7181479dad871018abdb3d7
1160300623 <48131190+1160300623@users.noreply.github.com>
2019-07-08 01:16:28 +0800

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,22 @@
+package hellowworld;
+
+
+
+public class hellowworld {
+
+
+
+  public static void main(String[] args) {
+
+
+    System.out.println("Hello World!");
+
+    System.out.println("1160300623 is ever here.");
+
+    System.out.println("1160300623 is ever here too.");
+
+ }
+
+
+
+}
